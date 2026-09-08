037f7496a23a7efca34766cff9665e35b9461ebc
ashwinsarathe <ashwinsarathe04@gmail.com>
2026-09-08 21:22:09 +0530

    Added CRUD methods in Student Service

--- src/main/java/com/example/sms/repository/StudentRepository.java
+++ src/main/java/com/example/sms/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.example.sms.repository;
 
 import com.example.sms.model.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
+    Optional<Student> getStudentById(String id);
 }
--- src/main/java/com/example/sms/service/StudentService.java
+++ src/main/java/com/example/sms/service/StudentService.java
@@ -1,7 +1,32 @@
 package com.example.sms.service;
 
+import com.example.sms.model.Student;
+import com.example.sms.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
+    StudentRepository studentRepository;
+    StudentService(StudentRepository studentRepository){
+        this.studentRepository = studentRepository;
+    }
+
+    public void createStudent(Student student){
+        studentRepository.save(student);
+    }
+    public Optional<Student> getStudentById(String id){
+        return studentRepository.getStudentById(id);
+    }
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+    public void deleteStudentById(String id){
+        Optional<Student> optionalStudent = studentRepository.getStudentById(id);
+        if(optionalStudent.isPresent()){
+            studentRepository.delete(optionalStudent.get());
+        }
+    }
 }
