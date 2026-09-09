d1f3d19258a73b0d87dff350aaf8d8ff72b55565
ashwinsarathe <ashwinsarathe04@gmail.com>
2026-09-09 20:40:35 +0530

    Created CRUD methods in DepartmentService

--- src/main/java/com/example/sms/service/DepartmentService.java
+++ src/main/java/com/example/sms/service/DepartmentService.java
@@ -1,7 +1,30 @@
 package com.example.sms.service;
 
+import com.example.sms.model.Department;
+import com.example.sms.repository.DepartmentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DepartmentService {
+    DepartmentRepository departmentRepository;
+    DepartmentService(DepartmentRepository departmentRepository){
+        this.departmentRepository=departmentRepository;
+    }
+
+    public void createDepartment(Department department){
+        departmentRepository.save(department);
+    }
+    public Optional<Department> getDepartmentById(String id){
+        return departmentRepository.findById(id);
+    }
+    public List<Department> getAllDepartments(){
+        return departmentRepository.findAll();
+    }
+    public void deleteDepartmentById(String id){
+        if(departmentRepository.existsById(id))
+            departmentRepository.deleteById(id);
+    }
 }
