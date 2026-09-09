ad771825d272111e3de47f2b0b88ac5d07b74a8d
ashwinsarathe <ashwinsarathe04@gmail.com>
2026-09-09 20:53:20 +0530

    Created CRUD methods in Course Service

--- src/main/java/com/example/sms/repository/CourseRepository.java
+++ src/main/java/com/example/sms/repository/CourseRepository.java
@@ -4,4 +4,5 @@ import com.example.sms.model.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CourseRepository extends MongoRepository<Course, String> {
+    boolean existsByCode(String code);
 }
--- src/main/java/com/example/sms/service/CourseService.java
+++ src/main/java/com/example/sms/service/CourseService.java
@@ -1,7 +1,34 @@
 package com.example.sms.service;
 
+import com.example.sms.model.Course;
+import com.example.sms.repository.CourseRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
+    CourseRepository courseRepository;
+    CourseService(CourseRepository courseRepository){
+        this.courseRepository=courseRepository;
+    }
+
+    public void createCourse(Course course){
+        String courseCode = course.getCode();
+        if(!courseRepository.existsByCode(courseCode)){
+            courseRepository.save(course);
+        }
+    }
+    public Optional<Course> getCourseById(String id){
+        return courseRepository.findById(id);
+    }
+    public List<Course> getAllCourses(){
+        return courseRepository.findAll();
+    }
+    public void deleteCourseById(String id){
+        if(courseRepository.existsById(id)){
+            courseRepository.deleteById(id);
+        }
+    }
 }
