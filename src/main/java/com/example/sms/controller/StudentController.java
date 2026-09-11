96315fddd735007bba9d9e992fee0f84087dfea6
ashwinsarathe <ashwinsarathe01@gmail.com>
2026-09-11 21:26:16 +0530

    Created Student Controller Methods

--- src/main/java/com/example/sms/controller/StudentController.java
+++ src/main/java/com/example/sms/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.example.sms.controller;
 
+import com.example.sms.model.Student;
 import com.example.sms.service.StudentService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController("student")
 public class StudentController {
@@ -11,4 +14,23 @@ public class StudentController {
         this.studentService=studentService;
     }
 
+    @PostMapping
+    public void createStudent(@RequestBody Student student){
+        studentService.createStudent(student);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Student> getStudentById(@PathVariable String id){
+        return studentService.getStudentById(id);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudentById(@PathVariable String id){
+        studentService.deleteStudentById(id);
+    }
 }
